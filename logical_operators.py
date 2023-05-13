15a88e8cca9838ffa432b0096750daa0e9023640
saurabh8883 <saurabhanj1485@gmail.com>
2023-05-13 17:41:14 +0530

    more update

--- logical_operators.py
+++ logical_operators.py
@@ -8,38 +8,56 @@
 # not (reverses the output)
 
 
-high_income = True
-good_credit = True
+# high_income = True
+# good_credit = True
 
-if high_income and good_credit:
-    print("You are eligible for the loan")
+# if high_income and good_credit:
+#     print("You are eligible for the loan")
 
+# else:
+#     print("Sorry try again after 90 days")
+
+# high_income = True
+# good_credit = False
+
+# if high_income and good_credit:
+#     print("You are eligible for the loan")
+
+# else:
+#     print("Sorry try again after 90 days")
+
+# high_income = True
+# good_credit = False
+
+# if high_income or good_credit:
+#     print("You are eligible for the loan")
+
+# else:
+#     print("Sorry try again after 90 days")
+
+# high_income = False
+# good_credit = False
+
+# if high_income or good_credit:
+#     print("You are eligible for the loan")
+
+# else:
+#     print("Sorry try again after 90 days")
+
+
+has_good_credit = True                                      # use of not operator 
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for the loan")
 else:
-    print("Sorry try again after 90 days")
+    print("Uneligible for the loan")
 
-high_income = True
-good_credit = False
 
-if high_income and good_credit:
-    print("You are eligible for the loan")
+has_good_credit = True                                      # use of not operator 
+has_criminal_record = False
 
+if has_good_credit and not has_criminal_record:
+    print("Eligible for the loan")
 else:
-    print("Sorry try again after 90 days")
-
-high_income = True
-good_credit = False
-
-if high_income or good_credit:
-    print("You are eligible for the loan")
-
-else:
-    print("Sorry try again after 90 days")
-
-high_income = False
-good_credit = False
-
-if high_income or good_credit:
-    print("You are eligible for the loan")
-
-else:
-    print("Sorry try again after 90 days")
+    print("Uneligible for the loan")
