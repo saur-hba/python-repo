8cd13ea1e09c9ea0812b455e2351e0d1b17eb00a
saurabh8883 <saurabhanj1485@gmail.com>
2023-05-13 18:09:24 +0530

    some basic code for task performance is added here

--- comparison_operator.py
+++ comparison_operator.py
@@ -1,17 +1,24 @@
-x = 3>2
-print(x)
+# x = 3>2
+# print(x)
 
-x = 3>=2
-print(x)
+# x = 3>=2
+# print(x)
 
-x = 3<2
-print(x)
+# x = 3<2
+# print(x)
 
-x = 3<=2
-print(x)
+# x = 3<=2
+# print(x)
 
-x = 3==2
-print(x)
+# x = 3==2
+# print(x)
 
-x = 3!=2
-print(x)
+# x = 3!=2
+# print(x)
+
+temp = 35
+
+if temp > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
